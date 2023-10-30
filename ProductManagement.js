5178cba93d47821b32386d6e836490ce789acaba
nitchankk <110713076+nitchankk@users.noreply.github.com>
2023-10-31 03:47:10 +0700

    Add files via upload

---
+++ ProductManagement.js
@@ -0,0 +1,41 @@
+//practice teacher
+
+function ProductManagement() {
+  //add products variable here 
+  const products = []
+  function getAllProducts() {
+    return products 
+  } 
+  function addProduct(product) {
+    products.push(product)
+  }
+
+  function searchByName(name) {
+    return products.filter((product) => product.name.toLowerCase().includes(name.toLowerCase()))
+
+  }
+
+  function searchByPriceRange(minPrice, maxPrice) {
+    return products.filter((product) => product.price > minPrice && maxPrice > product.price )
+  }
+
+  function removeAll() {
+    products.splice(0,products.length)
+  }
+
+  return {
+    removeAll,
+    searchByName,
+    searchByPriceRange,
+    getAllProducts,
+    addProduct
+  }
+}
+module.exports = ProductManagement
+const productCatalog = ProductManagement()
+
+
+//practice bz
+class Student {
+  
+}
